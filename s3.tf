246bc0cd739f1c1ff6231d0ebe98e138562389dc
Shivam Patel <157244434+shivxm03@users.noreply.github.com>
2024-06-15 23:41:49 -0400

    Initial commit

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "my.tf-state-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
+resource "aws_s3_bucket" "my_tf_state_bucket" {
+  bucket = "my.tf-state-bucket"  # Use the actual bucket name since it already exists
+  acl    = var.acl
+
+  versioning {
     enabled = var.versioning
   }
 }
