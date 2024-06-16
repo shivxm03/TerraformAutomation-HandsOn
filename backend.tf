357be31d8e5f256c63b715c7c4fe83c2453a0903
Shivam Patel <157244434+shivxm03@users.noreply.github.com>
2024-06-15 22:54:00 -0400

    Initial commit

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-bucket-for-tf-state"
+    bucket = "my.tf-state-bucket"
     key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    region = "ca-central-1"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
